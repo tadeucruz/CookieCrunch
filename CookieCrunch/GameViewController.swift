c3670e175b5fee402b8a520ab8faec0ad1cdf00d
tadeucruz <tadeucruz@gmail.com>
2015-05-06 22:32:44 -0300

    Código com base em:
    http://www.raywenderlich.com/75273/make-game-like-candy-crush-with-swift-tutorial-part-1
    http://www.raywenderlich.com/75273/make-game-like-candy-crush-with-swift-tutorial-part-2

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -1,69 +1,171 @@
-//
-//  GameViewController.swift
-//  CookieCrunch
-//
-//  Created by tadeucruz on 4/21/15.
-//  Copyright (c) 2015 tadeucruz. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
+import AVFoundation
 
-extension SKNode {
-    class func unarchiveFromFile(file : String) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
+
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+    
+    var scene: GameScene!
+    var level: Level!
+    
+    var movesLeft = 0
+    var score = 0
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var gameOverPanel: UIImageView!
+    @IBOutlet weak var shuffleButton: UIButton!
+    
+    
+    var tapGestureRecognizer: UITapGestureRecognizer!
+    
+    @IBAction func shuffleButtonPressed(AnyObject) {
+        shuffle()
+        decrementMoves()
     }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-        } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+    }
+    
+    override func viewDidLoad() {
+        gameOverPanel.hidden = true
+        super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        scene.swipeHandler = handleSwipe
+        
+        // Criando um level
+        level = Level(filename: "Levels/Level_1")
+        scene.level = level
+        scene.addTiles()
+        
+        // Present the scene.
+        skView.presentScene(scene)
+        backgroundMusic.play()
+        beginGame()
+    }
+    
+    func beginGame() {
+        movesLeft = level.maximumMoves
+        score = 0
+        updateLabels()
+        level.resetComboMultiplier()
+        scene.animateBeginGame() {
+            self.shuffleButton.hidden = false
+        }
+        shuffle()
+    }
+    
+    func shuffle() {
+        scene.removeAllCookieSprites()
+        let newCookies = level.shuffle()
+        scene.addSpritesForCookies(newCookies)
+    }
+    
+    func handleSwipe(swap: Swap) {
+        view.userInteractionEnabled = false
+        
+        if level.isPossibleSwap(swap) {
+            level.performSwap(swap)
+            //scene.animateSwap(swap, completion: handleMatches)
+            //scene.animateSwap(swap) {
+            scene.animateSwap(swap, completion: handleMatches)
+        } else {
+            scene.animateInvalidSwap(swap) {
+                self.view.userInteractionEnabled = true
+            }
+        }
+    }
+    
+    func handleMatches() {
+        let chains = level.removeMatches()
+        if chains.count == 0 {
+            beginNextTurn()
+            return
+        }
+        scene.animateMatchedCookies(chains) {
+            for chain in chains {
+                self.score += chain.score
+            }
+            self.updateLabels()
+            let columns = self.level.fillHoles()
+            self.scene.animateFallingCookies(columns) {
+                let columns = self.level.topUpCookies()
+                self.scene.animateNewCookies(columns) {
+                    //self.view.userInteractionEnabled = true
+                    self.handleMatches()
+                }
+            }
+        }
+    }
+    
+    func beginNextTurn() {
+        level.detectPossibleSwaps()
+        decrementMoves()
+        view.userInteractionEnabled = true
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(format: "%ld", level.targetScore)
+        movesLabel.text = String(format: "%ld", movesLeft)
+        scoreLabel.text = String(format: "%ld", score)
+    }
+    
+    func decrementMoves() {
+        if score >= level.targetScore {
+            gameOverPanel.image = UIImage(named: "LevelComplete")
+            showGameOver()
+        } else if movesLeft == 0 {
+            gameOverPanel.image = UIImage(named: "GameOver")
+            showGameOver()
+        }
+        --movesLeft
+        updateLabels()
+    }
+    
+    func showGameOver() {
+        shuffleButton.hidden = true
+        gameOverPanel.hidden = false
+        scene.userInteractionEnabled = false
+        
+        scene.animateGameOver() {
+            self.tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "hideGameOver")
+            self.view.addGestureRecognizer(self.tapGestureRecognizer)
+        }
+    }
+    
+    func hideGameOver() {
+        view.removeGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer = nil
+        
+        gameOverPanel.hidden = true
+        scene.userInteractionEnabled = true
+        
+        beginGame()
+    }
+    
+    lazy var backgroundMusic: AVAudioPlayer = {
+        let url = NSBundle.mainBundle().URLForResource("Sounds/Mining by Moonlight", withExtension: "mp3")
+        let player = AVAudioPlayer(contentsOfURL: url, error: nil)
+        player.numberOfLoops = -1
+        return player
+        }()
+    
 }
---
+++ CookieCrunch/Swap.swift
@@ -0,0 +1,23 @@
+struct Swap: Printable, Hashable{
+    let cookieA: Cookie
+    let cookieB: Cookie
+    
+    init(cookieA: Cookie, cookieB: Cookie) {
+        self.cookieA = cookieA
+        self.cookieB = cookieB
+    }
+    
+    var description: String {
+        return "swap \(cookieA) with \(cookieB)"
+    }
+    
+    var hashValue: Int {
+        return cookieA.hashValue ^ cookieB.hashValue
+    }
+}
+
+
+func ==(lhs: Swap, rhs: Swap) -> Bool {
+    return (lhs.cookieA == rhs.cookieA && lhs.cookieB == rhs.cookieB) ||
+        (lhs.cookieB == rhs.cookieA && lhs.cookieA == rhs.cookieB)
+}
